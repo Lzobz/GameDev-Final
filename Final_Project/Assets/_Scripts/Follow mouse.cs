450ae9062ae53e0c4a25bdb05a3f8bc1e41649e3
rthgct <rthgct@umsystem.edu>
2024-12-07 13:19:00 -0600

    Updated Follow mouse.css
    Added functionality to click the bee to start moving it

--- Final_Project/Assets/_Scripts/Follow mouse.cs
+++ Final_Project/Assets/_Scripts/Follow mouse.cs
@@ -8,6 +8,7 @@ public class Followmouse : MonoBehaviour
     Vector3 launchPos = new Vector3(0, 0, 0);
     public GameObject Bee;
     Rigidbody projectileRigidbody;
+    bool clickedBee = false;
     // Start is called before the first frame update
 
     private void Start()
@@ -16,11 +17,32 @@ public class Followmouse : MonoBehaviour
     }
     public void Update()
     {
-        Vector3 mousePos2D = Input.mousePosition;
-        mousePos2D.z = -Camera.main.transform.position.z;
-        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
-        Vector3 mouseDelta = mousePos3D - launchPos;
-        Vector3 projPos = launchPos + mouseDelta;  
-        Bee.transform.position = projPos;
+        if (Input.GetMouseButtonDown(0)) { //check for left mouse button click
+            //create ray from the camera to the mouse position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) { //perform ray cast
+
+                if (hit.collider.gameObject.tag == "Player") {
+                    Debug.Log("Clicked on player/bee!");
+                    clickedBee = true;
+                }
+
+            }
+
+        }
+
+        //move the bee if it has been clicked
+        if (clickedBee)
+        {
+            Vector3 mousePos2D = Input.mousePosition;
+            mousePos2D.z = -Camera.main.transform.position.z;
+            Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+            Vector3 mouseDelta = mousePos3D - launchPos;
+            Vector3 projPos = launchPos + mouseDelta;
+            Bee.transform.position = projPos;
+        }
     }
 }
