f3e6803007f45b095800f7d0daf65768e25798d3
rthgct <rthgct@umsystem.edu>
2024-12-07 20:58:53 -0600

    Fixed Merge Conflicts

--- Final_Project/Assets/_Scripts/TextScaler.cs
+++ Final_Project/Assets/_Scripts/TextScaler.cs
@@ -7,7 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class TextScaler : MonoBehaviour
 {
-<<<<<<< HEAD
     public TextMeshProUGUI text;
     public float scaleSpeed = 2f;
     public float minScale = 1.0f;
@@ -20,37 +19,13 @@ public class TextScaler : MonoBehaviour
         {
             text = GetComponent<TextMeshProUGUI>();
         }
-=======
-    [Header("Inscribed")]
-    public TextMeshProUGUI uitLevel;
-    public int level;
-    public int lives;
-    // Start is called before the first frame update
-    void Start()
-    {
-        level = 1;
-        lives = 3;
->>>>>>> 79372bd16038b2f816645c4262c449200fe39889
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         float scale = Mathf.Lerp(minScale, maxScale, ((Mathf.Sin(Time.time * scaleSpeed) + 1) / 2));
 
         text.transform.localScale = Vector3.one * scale;
-
-=======
-        uitLevel.text = "Level: 1 of 4";
-    
-        if (balloon.ballooncnt > 11)
-        {
-            SceneManager.LoadScene("Test Scene");
-
-            uitLevel.text = "Level: 2 of 4";
-
-        }
->>>>>>> 79372bd16038b2f816645c4262c449200fe39889
     }
 }
