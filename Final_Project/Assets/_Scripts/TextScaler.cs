37f5e4cbf159554c4f6b47011da0b2ae98d1a06e
rthgct <rthgct@umsystem.edu>
2024-12-07 20:42:59 -0600

    Small changes to Title Screen
    Made the yellow text scale small and large.

--- Final_Project/Assets/_Scripts/TextScaler.cs
+++ Final_Project/Assets/_Scripts/TextScaler.cs
@@ -5,17 +5,26 @@ using TMPro;
 
 public class TextScaler : MonoBehaviour
 {
-
+    public TextMeshProUGUI text;
+    public float scaleSpeed = 2f;
+    public float minScale = 1.0f;
+    public float maxScale = 1.2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (text == null)
+        {
+            text = GetComponent<TextMeshProUGUI>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float scale = Mathf.Lerp(minScale, maxScale, ((Mathf.Sin(Time.time * scaleSpeed) + 1) / 2));
+
+        text.transform.localScale = Vector3.one * scale;
+
     }
 }
