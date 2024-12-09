89f563326811d3df4e1dda2c3bf389e6a40b98ab
rthgct <rthgct@umsystem.edu>
2024-12-09 15:09:21 -0600

    Made Timer Persist Thru All 4 Scenes

--- Final_Project/Assets/_Scripts/Timer.cs
+++ Final_Project/Assets/_Scripts/Timer.cs
@@ -8,9 +8,17 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] float remainingTime;
+    [SerializeField] public static float remainingTime;
     [SerializeField] TextMeshProUGUI timerText;
 
+
+    private void Awake()
+    {
+        if (SceneManager.GetActiveScene().name == "Level1")
+        {
+            remainingTime = 240f;
+        }
+    }
 
     // Update is called once per frame
     void Update()
